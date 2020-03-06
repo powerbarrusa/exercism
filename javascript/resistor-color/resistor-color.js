069e21348b66e1a1ce4a3a9b413b9accad078257
powerbarrusa <barr@covrigaru.com>
2020-03-06 13:34:54 -0700

    reformatting to not allow anything but what's included in array.

--- javascript/resistor-color/resistor-color.js
+++ javascript/resistor-color/resistor-color.js
@@ -1,5 +1,9 @@
-export const colorCode = (color, colors = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]) => {
-  return colors.indexOf(color)
+export const colorCode = (color, COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]) => {
+  for (let i = 0; i < color.length; i++){
+    if (COLORS[i].includes(color)){
+      return COLORS.indexOf(color)
+    }
+  }
 }
 
-export const COLORS = undefined;
+export const COLORS = undefined
