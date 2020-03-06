72bc8cad3e2875b69ae474620a7aba19948d2519
powerbarrusa <barr@covrigaru.com>
2020-03-06 13:13:55 -0700

    changes from mentor. not sure what L6 is, but changed to defining implicitly the colors array

--- javascript/resistor-color/resistor-color.js
+++ javascript/resistor-color/resistor-color.js
@@ -1,5 +1,4 @@
-export const colorCode = (color) => {
-  let colors = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
+export const colorCode = (color, colors = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]) => {
   return colors.indexOf(color)
 }
 
