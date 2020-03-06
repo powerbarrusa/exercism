be60c1cd40b848263e34edbb0f0c30d868beab3b
powerbarrusa <barr@covrigaru.com>
2020-03-06 14:04:18 -0700

    it was right before. minor mods. waiting and seeing...

--- javascript/resistor-color/resistor-color.js
+++ javascript/resistor-color/resistor-color.js
@@ -1,9 +1,5 @@
-export const colorCode = (color, COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]) => {
-  for (let i = 0; i < color.length; i++){
-    if (COLORS[i].includes(color)){
+export const colorCode = (color) => {
       return COLORS.indexOf(color)
     }
-  }
-}
 
-export const COLORS = undefined
+export const COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
