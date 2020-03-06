ed74b21a12a24b1d59de68e4d0920c157f731017
powerbarrusa <barr@covrigaru.com>
2020-03-06 12:56:12 -0700

    successfully passing. waiting for mentor comments.

--- javascript/resistor-color/resistor-color.js
+++ javascript/resistor-color/resistor-color.js
@@ -1,5 +1,6 @@
-export const colorCode = () => {
-  
+export const colorCode = (color) => {
+  let colors = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
+  return colors.indexOf(color)
 }
 
 export const COLORS = undefined;
