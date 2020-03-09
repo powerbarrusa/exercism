27d31bd3172246383cba0077866c11cb36e1a141
powerbarrusa <barr@covrigaru.com>
2020-03-08 21:58:03 -0600

    challenge solved. Now to see what mentor thinks. This seems a bit busy and too complex for what's asked..

--- javascript/resistor-color-duo/resistor-color-duo.js
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -1,3 +1,9 @@
-export const decodedValue = () => {
-  
+export const decodedValue = (duo) => {
+  let color1 = COLORS.indexOf(duo[0])
+  let color2 = COLORS.indexOf(duo[1])
+  let digits = color1 + "" + color2
+  let trueDigits = Number(digits)
+  return trueDigits
 }
+
+export const COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
