6775430dc3f18eda65b63d152876ecf1aa5bb0b5
powerbarrusa <barr@covrigaru.com>
2020-03-12 10:01:30 -0600

    changes

--- javascript/resistor-color-duo/resistor-color-duo.js
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -1,9 +1,7 @@
 export const decodedValue = ([color1, color2]) => {
   let code1 = COLORS.indexOf(color1)
   let code2 = COLORS.indexOf(color2)
-  let digits = code1 * 10 + code2
-  let trueDigits = Number(digits)
-  return trueDigits
+  return  code1 * 10 + code2
 }
 
 export const COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
