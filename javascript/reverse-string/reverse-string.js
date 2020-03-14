6423f233a33edf4cea8209d310182e154c9f0c02
powerbarrusa <barr@covrigaru.com>
2020-03-13 22:39:23 -0600

    not sure why lowercase..

--- javascript/reverse-string/reverse-string.js
+++ javascript/reverse-string/reverse-string.js
@@ -1,8 +1,7 @@
 export const reverseString = (string) => {
-  let lower = string.toLowerCase()
-  let reverse = ""
-  for (let i = lower.length + 1; i > 0; i--){
-    reverse.concat(lower[i])
+  const Reverse = ""
+  for (let i = string.length + 1; i > 0; i--){
+    Reverse.concat(string[i])
   }
-  return reverse
+  return Reverse
 }
