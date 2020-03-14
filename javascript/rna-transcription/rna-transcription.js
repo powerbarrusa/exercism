5664254ea34e02a4469836f96e0bdc59f0d02df0
powerbarrusa <barr@covrigaru.com>
2020-03-14 11:58:39 -0600

    all tests pass

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -1,3 +1,18 @@
-export const toRna = () => {
-  
-}
+export const toRna = (dna) => {
+  let rna = ""
+  for (let i = 0; i < dna.length; i++){
+    if (dna[i] === "G"){
+      rna+= "C"
+    }
+    else if (dna[i] === "C"){
+      rna+= "G"
+    }
+    else if (dna[i] === "T"){
+      rna+= "A"
+    }
+    else if (dna[i] === "A"){
+      rna+= "U"
+    }
+  }
+    return rna
+  }
