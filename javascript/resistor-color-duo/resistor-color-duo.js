418d4cf2bf4aac854263c2538c884eca5babada6
powerbarrusa <barr@covrigaru.com>
2020-03-19 22:53:58 -0600

    last commit message was for this. refactoring this yada yada.

--- javascript/resistor-color-duo/resistor-color-duo.js
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -1,7 +1,7 @@
 export const decodedValue = ([color1, color2]) => {
   let code1 = COLORS.indexOf(color1)
   let code2 = COLORS.indexOf(color2)
-  return  code1 * 10 + code2
+  return code1 * 10 + code2
 }
 
 export const COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -1,5 +1,11 @@
 export const toRna = (dna) => {
   let rna = ""
+  const transcription = {
+    G: "C",
+    C: "G",
+    T: "A",
+    A: "U"
+  }
   for (let i = 0; i < dna.length; i++){
     if (dna[i] === "G"){
       rna+= "C"
