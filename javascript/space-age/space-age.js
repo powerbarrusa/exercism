07570d5c543db9070ceac52f6b45b6cc861bd324
powerbarrusa <barr@covrigaru.com>
2020-04-09 15:07:16 -0600

    back on. playing around

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -1,8 +1,17 @@
-//
-// This is only a SKELETON file for the 'Space Age' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export const age = () => {
-  throw new Error("Remove this statement and implement this function");
-};
+  let planets = {
+    Mercury: 0.2408467,
+    Venus: 0.61519726,
+    Earth: 1.0,
+    Mars: 1.8808158,
+    Jupiter: 11.862615,
+    Saturn: 29.447498,
+    Uranus: 84.016846,
+    Neptune: 164.79132
+  }
+  let seconds = {}
+  for (const prop in planets) {
+    seconds = (`${prop}: ${planets[prop] * 31557600}`)
+  }
+  console.log(seconds)
+}
