21b438cf99a90e5e6e2bc433668e162779b1c63c
powerbarrusa <barr@covrigaru.com>
2020-04-29 13:45:42 -0600

    passing for earth. optimizing for all

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -1,17 +1,15 @@
-export const age = (seconds) => {
-  let planetsInEarthYears = {
-    Mercury: 0.2408467,
-    Venus: 0.61519726,
-    Earth: 1.0,
-    Mars: 1.8808158,
-    Jupiter: 11.862615,
-    Saturn: 29.447498,
-    Uranus: 84.016846,
-    Neptune: 164.79132
+export const age = (
+  planet,
+  seconds,
+  planetsInEarthYears = {
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    earth: 1.0,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132
+  }) => {
+    return planetsInEarthYears[planet] = +((seconds / 31557600).toFixed(2))
   }
-  let planetsInEarthSeconds = {}
-  for (const prop in planetsInEarthYears) {
-    planetsInEarthSeconds[prop] = planetsInEarthYears[prop] * 31557600
-  }
-  return planetsInEarthSeconds
-}
