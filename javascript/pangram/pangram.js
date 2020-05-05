5629bd684b169621be26732c04ef4ce2cf819180
powerbarrusa <barr@covrigaru.com>
2020-05-05 01:20:18 -0600

    forced a pass on the first. making moves

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -1,3 +1,8 @@
-export const isPangram = () => {
-  
+export const isPangram = (pang, alpha = "abcdefghijklmnopqrstuvwxyz") => {
+  for (let i = 0; i < alpha.length; i++){
+    // console.log(alpha[i])
+    if (pang.includes("")){
+      return false
+    }
+  }
 }
